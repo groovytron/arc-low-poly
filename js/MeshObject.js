e883491ee4eb02595110204f6417be00e6f37a9d
Julien M'Poy <julien.mpoy@gmail.com>
2017-01-30 16:27:56 +0100

    Created a MeshObject responsible for a part of the obj loading

---
+++ js/MeshObject.js
@@ -0,0 +1,105 @@
+class MeshObject {
+    constructor(objFileName, textureFileName, x, y, z) {
+        this.modelViewMatrix = mat4.create();
+        this.projectionMatrix = mat4.create();
+        this.nMatrix = mat4.create(); // No idea what is is but we need it
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.loadModel(objFileName);
+        // initTextureWithImage(textureFileName, texColorTab);
+    }
+
+    handleOBJModel(filename, data){
+        console.info(filename + ' has been retrieved from the server');
+
+        var objData = new OBJ.Mesh(data);
+        this.vertexBuffer = getVertexBufferWithVertices(objData.vertices);
+        this.normalsBuffer = getVertexBufferWithVertices(objData.vertexNormals);
+        this.textureBuffer = getVertexBufferWithVertices(objData.textures);
+        this.indexBuffer = getIndexBufferWithIndices(objData.indices);
+        this.indices = objData.indices;
+        console.log("Indices", this.indices);
+    }
+
+    showPayloadInfo(filename, payload ) {
+        console.info(filename + ' has been retrieved from the server');
+        console.info("v: " + payload.vertices);
+        console.info("i: " + payload.indices);
+        console.info("c: " + payload.colors);
+
+        var passTable = [];
+        passTable = payload.indices;
+
+        console.info("#polygones : " + passTable.length / 3);
+    }
+
+    /**
+    * Creates the buffers that contain the geometry of the model
+    */
+    handleJSONModel(filename, payload) {
+        showPayloadInfo(filename, payload);
+
+        this.indices = payload.indices;
+
+        //initializes buffers: sends data from the JavaScript arrays to the graphics card
+        this.vertexBuffer = getVertexBufferWithVertices(payload.vertices);
+        this.normalsBuffer = getVertexBufferWithVertices(payload.normals);
+        this.indexBuffer = getIndexBufferWithIndices(payload.indices);
+    }
+
+    /**
+    * Creates an AJAX request to load a model asynchronously
+    */
+    loadModel(filename){
+        var request = new XMLHttpRequest();
+        console.info('Requesting ' + filename);
+        request.open("GET",filename);
+
+        request.onreadystatechange = () => {
+          if (request.readyState == 4) {
+            if(request.status == 404) {
+                console.info(filename + ' does not exist');
+             }
+            else {
+                var re = /(?:\.([^./]+))?$/;
+                var ext = re.exec(filename)[1];
+                console.info('file: '+ filename + ', ext: ' + ext);
+                switch( ext ){
+                    case "json": this.handleJSONModel(filename, JSON.parse(request.responseText)); break;
+                    case "obj":  this.handleOBJModel(filename, request.responseText); break;
+                    default: console.info("unknown format extension: " + ext );
+                }
+            }
+          }
+        }
+        request.send();
+    }
+
+    draw(){
+        mat4.identity(this.modelViewMatrix);
+
+        //Translates the mv matrix
+        mat4.translate(this.modelViewMatrix, this.modelViewMatrix, vec3.fromValues(this.x, this.y, this.z));
+        //Multiplies the model View matrix of the object with the view matrix of the scene
+        mat4.multiply(this.modelViewMatrix, this.modelViewMatrix, mvMatrix);
+        mat4.multiply(this.modelViewMatrix, translationMat, mvMatrix);
+
+        glContext.uniformMatrix4fv(prg.mvMatrixUniform, false, this.modelViewMatrix);
+        mat4.copy(this.nMatrix, this.modelViewMatrix);
+        mat4.invert(this.nMatrix, this.nMatrix);
+        mat4.transpose(this.nMatrix, this.nMatrix);
+        glContext.uniformMatrix4fv(prg.nMatrixUniform, false, this.nMatrix);
+        glContext.bindBuffer(glContext.ARRAY_BUFFER, this.vertexBuffer);
+        glContext.vertexAttribPointer(prg.vertexPositionAttribute, 3, glContext.FLOAT, false, 0, 0);
+        glContext.bindBuffer(glContext.ARRAY_BUFFER, this.normalsBuffer);
+        glContext.vertexAttribPointer(prg.vertexNormalAttribute, 3, glContext.FLOAT, false, 0, 0);
+        glContext.bindBuffer(glContext.ARRAY_BUFFER, this.textureBuffer);
+        glContext.vertexAttribPointer(prg.textureCoordsAttribute, 2, glContext.FLOAT, false, 0, 0);
+        glContext.bindBuffer(glContext.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+        glContext.activeTexture(glContext.TEXTURE0);
+        glContext.bindTexture(glContext.TEXTURE_2D, texColorTab[0]);
+        glContext.uniform1i(prg.colorTextureUniform, 0);
+        glContext.drawElements(glContext.TRIANGLES, this.indices.length, glContext.UNSIGNED_SHORT,0);
+    }
+}
--- js/app.js
+++ js/app.js
@@ -14,6 +14,8 @@ var ty = 0.2;
 var tz = -10.5;
 window.onkeydown = checkKey;
 var movingStep = 1.0;
+var meshObjects = [];
+
 function checkKey(ev){
     switch(ev.keyCode){
         case 87: tz+=movingStep; break;
@@ -28,75 +30,53 @@ function checkKey(ev){
     }
 }
 function initShaderParameters(prg){
-    prg.vertexPositionAttribute     = glContext.getAttribLocation(prg, "aVertexPosition");
+    prg.vertexPositionAttribute = glContext.getAttribLocation(prg, "aVertexPosition");
     glContext.enableVertexAttribArray(prg.vertexPositionAttribute);
-    prg.vertexNormalAttribute         = glContext.getAttribLocation(prg, "aVertexNormal");
+    prg.vertexNormalAttribute = glContext.getAttribLocation(prg, "aVertexNormal");
     glContext.enableVertexAttribArray(prg.vertexNormalAttribute);
-    prg.textureCoordsAttribute         = glContext.getAttribLocation(prg, "aTextureCoord");
+    prg.textureCoordsAttribute = glContext.getAttribLocation(prg, "aTextureCoord");
     glContext.enableVertexAttribArray(prg.textureCoordsAttribute);
-    prg.colorTextureUniform         = glContext.getUniformLocation(prg, "uColorTexture");
-    prg.pMatrixUniform                 = glContext.getUniformLocation(prg, 'uPMatrix');
-    prg.mvMatrixUniform                = glContext.getUniformLocation(prg, 'uMVMatrix');
-    prg.nMatrixUniform                 = glContext.getUniformLocation(prg, 'uNMatrix');
-    prg.lightPositionUniform           = glContext.getUniformLocation(prg, 'uLightPosition');
+    prg.colorTextureUniform = glContext.getUniformLocation(prg, "uColorTexture");
+    prg.pMatrixUniform = glContext.getUniformLocation(prg, 'uPMatrix');
+    prg.mvMatrixUniform = glContext.getUniformLocation(prg, 'uMVMatrix');
+    prg.nMatrixUniform = glContext.getUniformLocation(prg, 'uNMatrix');
+    prg.lightPositionUniform = glContext.getUniformLocation(prg, 'uLightPosition');
 }
 
 function initScene(){
-    // TODO: Implement Me
-    // Create billboard(s)
-    console.log("Il faut créer un billboard!");
+    meshObjects.push(new MeshObject("models/OBJ/Table/Table-Final-uvmapped.obj", "models/OBJ/Table/uvmap.png", 0.0, 0.0, 0.0));
+    meshObjects.push(new MeshObject("models/OBJ/Table/Table-Final-uvmapped.obj", "models/OBJ/Table/uvmap.png", 1.0, 1.0, 1.0));
+    initLights();
+    initTextureWithImage("models/OBJ/Table/uvmap.png", texColorTab);
 }
 // Might be unuseful
 function initLights(){
     glContext.uniform3f(prg.lightPositionUniform, 0, 0, 1);
 }
-function drawObject(modelViewMatrix, vertexBuffer, normalsBuffer, textureBuffer, indexBuffer, indexCount){
-    glContext.uniformMatrix4fv(prg.mvMatrixUniform, false, modelViewMatrix);
-    mat4.copy(nMatrix, modelViewMatrix);
-    mat4.invert(nMatrix, nMatrix);
-    mat4.transpose(nMatrix, nMatrix);
-    glContext.uniformMatrix4fv(prg.nMatrixUniform, false, nMatrix);
-    glContext.bindBuffer(glContext.ARRAY_BUFFER, vertexBuffer);
-    glContext.vertexAttribPointer(prg.vertexPositionAttribute, 3, glContext.FLOAT, false, 0, 0);
-    glContext.bindBuffer(glContext.ARRAY_BUFFER, normalsBuffer);
-    glContext.vertexAttribPointer(prg.vertexNormalAttribute, 3, glContext.FLOAT, false, 0, 0);
-    glContext.bindBuffer(glContext.ARRAY_BUFFER, textureBuffer);
-    glContext.vertexAttribPointer(prg.textureCoordsAttribute, 2, glContext.FLOAT, false, 0, 0);
-    glContext.bindBuffer(glContext.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    glContext.activeTexture(glContext.TEXTURE0);
-    glContext.bindTexture(glContext.TEXTURE_2D, texColorTab[0]);
-    glContext.uniform1i(prg.colorTextureUniform, 0);
-    glContext.drawElements(glContext.TRIANGLES, indexCount, glContext.UNSIGNED_SHORT,0);
-}
+
 function startRenderLoop(){
-    initLights(); // Remove if unused
     mat4.identity(mvMatrix); // Must be done in drawScene()
     renderLoop();
 }
 function drawScene(){
     glContext.clearColor(0.4, 0.4, 1.0, 1.0);
-    // glContext.clearColor(1.0, 1.0, 1.0, 1.0);
-    // glContext.enable(glContext.DEPTH_TEST);
     glContext.blendFunc(glContext.ONE, glContext.ONE_MINUS_SRC_ALPHA);
     glContext.enable(glContext.BLEND);
+    // glContext.disable(glContext.BLEND);
     glContext.clear(glContext.COLOR_BUFFER_BIT | glContext.DEPTH_BUFFER_BIT);
     glContext.viewport(0, 0, c_width, c_height);
     mat4.perspective(pMatrix, degToRad(60), c_width / c_height, 0.1, 1000.0);
     glContext.uniformMatrix4fv(prg.pMatrixUniform, false, pMatrix);
-    if(indicesArray.length > 0){
-        translationMat = mat4.create();
-        mat4.identity(translationMat);
-        mat4.translate(translationMat, translationMat, [tx, ty, tz]);
-        rotateModelViewMatrixUsingQuaternion(true);
-        var modelViewMatrix = mat4.multiply(mat4.create(), translationMat, mvMatrix);
-        drawObject(modelViewMatrix, vertexBuffersArray[0], normalBuffersArray[0], textureBuffersArray[0], indexBuffersArray[0], indicesArray[0].length);
-    }
+
+    translationMat = mat4.create();
+    mat4.identity(translationMat);
+    mat4.translate(translationMat, translationMat, [tx, ty, tz]);
+    rotateModelViewMatrixUsingQuaternion(true);
+    meshObjects.forEach( m => m.draw());
 }
 function initWebGL(){
     glContext = getGLContext('webgl-canvas');
     initProgram();
     initScene();
-    loadModel("models/OBJ/Table/Table-Final-uvmapped.obj"); // Must be done in Billboard class
-    initTextureWithImage( "models/OBJ/Table/uvmap.png", texColorTab ); // Must be done in Billboard class
     startRenderLoop(); // Might be reaplced by renderLoop()
 }
